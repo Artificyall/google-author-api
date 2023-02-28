49812559a5fe7a00bd419540db5453c376becfbd
dbriend <donovan.briend@etu.u-bordeaux.fr>
2023-02-28 12:13:16 +0100

    aa

--- src/BookList.js
+++ src/BookList.js
@@ -6,18 +6,25 @@ import Book from "./Book";
 export const BookList = () => {
   const [books, setBooks] = useState([]);
   const [query, setQuery] = useState("penguinos");
+  const [page, setPage] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
 
   useEffect(() => {
     if (!query) return;
     axios
-      .get(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${query}`)
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=inauthor:${query}&startIndex=${
+          page * 10
+        }`
+      )
       .then((response) => {
         setBooks(response.data.items);
+        setTotalItems(response.data.totalItems);
       })
       .catch((error) => {});
-  }, [query]);
+  }, [query, page]);
 
-/*        <button type="submit">Rechercher</button> */
+  /*        <button type="submit">Rechercher</button> */
 
   return (
     <div className="App">
@@ -25,6 +32,7 @@ export const BookList = () => {
         onSubmit={(event) => {
           event.preventDefault();
           setQuery(event.target.elements.query.value);
+          setPage(0);
         }}
       >
         <input type="text" name="query" placeholder="Penguinos..." />
@@ -33,11 +41,22 @@ export const BookList = () => {
         <ul>
           {books.map((book) => (
             <li key={book.id}>
-              <Book book={book} />
+              <a href={book.volumeInfo.previewLink}>
+                <Book book={book} />
+              </a>
             </li>
           ))}
         </ul>
       )}
+      <div className="pagination">
+        <button disabled={page === 0} onClick={() => setPage(page - 1)}>
+          Prev
+        </button>
+        <p>
+          Page {page + 1} / {totalItems} books
+        </p>
+        <button onClick={() => setPage(page + 1)}>Next</button>
+      </div>
     </div>
   );
 };
